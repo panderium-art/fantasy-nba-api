2526b7d4d0488e191f72936e42f516624b5a1ed0
Artsiom Mialkumau <melkumov.artem@gmail.com>
2024-05-07 10:06:17 +0200

    feat: update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,31 +4,39 @@ import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  // Create 10 users
-  for (let i = 0; i < 10; i++) {
-    await prisma.user.create({
-      data: {
-        email: faker.internet.email(),
-        password: faker.internet.password(), // Remember to hash passwords in real applications
-      },
-    });
-  }
+const TIMEOUT_MS = 5000;
 
-  // Create 10 teams
-  for (let i = 0; i < 10; i++) {
-    await prisma.team.create({
-      data: {
-        team_name: faker.company.name(),
-        wins: faker.number.int({max: 82}),
-        losses: faker.number.int({max: 82}),
-        home_city: faker.location.city(),
-        home_arena: faker.company.name(),
-        championships_amount: faker.number.int({max: 10}),
-        win_loss_ratio: faker.number.float({ min: 0, max: 82 }),
-      },
-    });
-  }
+async function main() {
+
+  // using timeout here to give prisma time to generate client after db reset
+  // needed if used in npm run prisma:reset:seed
+  setTimeout(async () => {
+    // Create 10 users
+    for (let i = 0; i < 10; i++) {
+      await prisma.user.create({
+        data: {
+          email: faker.internet.email(),
+          password: faker.internet.password(), // Remember to hash passwords in real applications
+        },
+      });
+    }
+
+    // Create 10 teams
+    for (let i = 0; i < 10; i++) {
+      await prisma.team.create({
+        data: {
+          team_name: faker.company.name(),
+          wins: faker.number.int({max: 82}),
+          losses: faker.number.int({max: 82}),
+          home_city: faker.location.city(),
+          home_arena: faker.company.name(),
+          championships_amount: faker.number.int({max: 10}),
+          win_loss_ratio: faker.number.float({ min: 0, max: 82 }),
+        },
+      });
+    }    
+  }, TIMEOUT_MS)
+
 }
 
 main()
