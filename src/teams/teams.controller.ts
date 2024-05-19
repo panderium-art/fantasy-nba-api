fe6e64eca7df6711bc77aeae8384569bb724c502
Artsiom Mialkumau <melkumov.artem@gmail.com>
2024-05-19 23:20:40 +0200

    docs: swagger update for teams create/update/delete

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -10,7 +10,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { TeamsService } from './teams.service';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RoleGuard } from 'src/auth/guards/role.guard';
 import { Roles } from 'src/auth/decorators/role.decorator';
 
@@ -38,8 +38,31 @@ export class TeamsController {
 
   @Post()
   @ApiOperation({ summary: 'Create team' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        team_name: { type: 'string' },
+        wins: { type: 'number' },
+        losses: { type: 'number' },
+        win_loss_ratio: { type: 'number' },
+        home_city: { type: 'string' },
+        home_arena: { type: 'string' },
+        championships_amount: { type: 'number' },
+      },
+      example: {
+        team_name: 'Dallas Mavericks',
+        wins: 62,
+        losses: 20,
+        win_loss_ratio: 3.4,
+        home_city: 'Dallas',
+        home_arena: 'Dallas Arena',
+        championships_amount: 5,
+      },
+    },
+  })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
-  @ApiResponse({ status: 200, description: 'OK', type: 'object' })
+  @ApiResponse({ status: 200, description: 'OK' })
   @Roles('ADMIN')
   create(@Body() createTeamDto: any) {
     // Define DTO based on your schema
@@ -48,8 +71,26 @@ export class TeamsController {
 
   @Put(':id')
   @ApiOperation({ summary: 'Update specific team by id' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      example: {
+        home_arena: 'Dallas Mavericks Arena',
+        championships_amount: 3,
+      },
+      anyOf: [
+        { required: ['team_name'] },
+        { required: ['wins'] },
+        { required: ['losses'] },
+        { required: ['win_loss_ratio'] },
+        { required: ['home_city'] },
+        { required: ['home_arena'] },
+        { required: ['championships_amount'] },
+      ],
+    },
+  })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
-  @ApiResponse({ status: 200, description: 'OK', type: 'object' })
+  @ApiResponse({ status: 200, description: 'OK'})
   @Roles('ADMIN')
   update(@Param('id') id: string, @Body() updateTeamDto: any) {
     // Define DTO based on your schema
@@ -59,7 +100,7 @@ export class TeamsController {
   @Delete(':id')
   @ApiOperation({ summary: 'Delete specific team by id' })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
-  @ApiResponse({ status: 200, description: 'OK', type: 'object' })
+  @ApiResponse({ status: 200, description: 'OK'})
   @Roles('ADMIN')
   remove(@Param('id') id: string) {
     return this.teamsService.remove(id);
